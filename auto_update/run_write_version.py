49306eaf1ae266b5d919e6b60e4c8164713f11ec
SakuraFox <982001940@qq.com>
2025-01-23 15:34:36 +0800

    初始化

---
+++ auto_update/run_write_version.py
@@ -0,0 +1,23 @@
+"""
+@Author : SakuraFox
+@Time: 2025-01-23 15:18
+@File : run_write_version.py
+@Description : file description
+"""
+# -*- coding: utf-8 -*-
+import os
+
+
+version = os.environ.get('version')
+print("version:", version)
+# 获取运行目录
+# print("run dir:", os.getcwd())
+# 获取文件目录
+fileDir = os.path.dirname(__file__)
+print("file dir:", os.path.dirname(__file__))
+versionFilePath = os.path.join(fileDir, "version.py")
+print("edit file {versionFilePath} output: version = {version}")
+# 覆盖写出文件 version.py 中
+with open(versionFilePath, 'w') as f:
+    f.write(f'version = "{version}"')
+exit()
